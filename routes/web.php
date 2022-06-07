1c83af775534af1af4b4ff8fdc8aaf6ef638b8eb
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-06-07 08:11:42 +0100

    Custom header, headers, wildcard, constraints, helper methods...

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+namespace App\routes;
+
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +19,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return response('<h1>Hello World</h1>', 200)
+        ->header('Content-Type', 'text/plain')
+        ->header('foo', 'bar');
+});
+
+Route::get('/posts/{id}', function ($id) {
+    return response('Post ' . $id);
+})->where('id', '[0-9]+');
+
+
+Route::get('/search/', function (Request $request) {
+    dd($request);
+});
+
+Route::get('/debug/{id}', function ($id) {
+    ddd($id);
+    return response('Post ' . $id);
+})->where('id', '[0-9]+');
