2b3a76c93a3868bdf0f8d5a3ce3fd82994e3de39
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-06-08 12:55:52 +0100

    Create an Eloquent Model

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,38 +2,10 @@
 
 namespace App\Models;
 
-class Listing
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Listing extends Model
 {
-    public static function all()
-    {
-        return [
-
-            [
-                'id' => 1,
-                'title' => 'Listing One',
-                'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem 
-                cum perferendis sapiente, reiciendis sit aliquam. Sit repudiandae optio et laborum, 
-                eveniet ipsum quos dicta earum ratione veniam molestiae dolor omnis.'
-            ],
-
-            [
-                'id' => 2,
-                'title' => 'Listing Two',
-                'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem 
-                cum perferendis sapiente, reiciendis sit aliquam. Sit repudiandae optio et laborum, 
-                eveniet ipsum quos dicta earum ratione veniam molestiae dolor omnis.'
-            ]
-        ];
-    }
-
-    public static function find($id)
-    {
-        $listings = self::all();
-
-        foreach ($listings as $listing) {
-            if ($listing['id'] == $id) {
-                return $listing;
-            }
-        }
-    }
+    use HasFactory;
 }
