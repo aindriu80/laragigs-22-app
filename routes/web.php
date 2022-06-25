a029550663a60f58590397a52057d500c2e74236
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-06-25 20:31:15 +0100

    Logout functionality

--- routes/web.php
+++ routes/web.php
@@ -52,6 +52,9 @@ Route::get('/register', [UserController::class, 'create']);
 // Create New User
 Route::post('/users', [UserController::class, 'store']);
 
+// Logout functionality
+Route::post('/logout', [UserController::class, 'logout']);
+
 // Common Resource Routes:
 // index - Show all listings
 // show - Show single listing
